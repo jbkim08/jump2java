004e953e665abdf0fad8c1f9628fb215d007a343
JBKIM <drv98@naver.com>
2024-03-18 12:43:42 +0900

    최초커밋

---
+++ src/ch03_1/S_숫자.java
@@ -0,0 +1,12 @@
+package ch03_1;
+
+public class S_숫자 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
