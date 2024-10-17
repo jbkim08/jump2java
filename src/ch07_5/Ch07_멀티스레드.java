4aed5e232c1c452a3cae03054273323f4e57c81c
JB <drv98@naver.com>
2024-10-17 12:24:50 +0900

    스레드

---
+++ src/ch07_5/Ch07_멀티스레드.java
@@ -0,0 +1,32 @@
+package ch07_5;
+
+class Sample2 extends Thread {
+
+    int n;
+
+    public Sample2(int n) {
+        this.n = n;
+    }
+
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(this.n +"스레드 : " + i);
+            try {
+                Thread.sleep(500); //0.5초 대기
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class Ch07_멀티스레드 {
+    public static void main(String[] args) {
+        //스레드 객체 2개
+        Thread t1 = new Sample2(1);
+        Thread t2 = new Sample2(2);
+        t1.start();
+        t2.start();
+        System.out.println("Main thread 종료");
+    }
+}
